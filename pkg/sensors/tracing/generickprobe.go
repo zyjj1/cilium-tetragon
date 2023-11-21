9e40d1eb36ca1adcd695f74c8cee9040927e9618
Jiri Olsa <jolsa@kernel.org>
2023-11-21 21:13:54 +0000

    tetragon: Add support to store return action
    Storing the return action value to final merged kprobe event.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- pkg/sensors/tracing/generickprobe.go
+++ pkg/sensors/tracing/generickprobe.go
@@ -1656,6 +1656,7 @@ func retprobeMerge(prev pendingEvent, curr pendingEvent) *tracing.MsgGenericKpro
 			enterEv.Args = append(enterEv.Args, retArg)
 		}
 	}
+	enterEv.ReturnAction = retEv.Action
 	return enterEv
 }
 
