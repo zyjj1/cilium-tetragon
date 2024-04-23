bbd308456393dcc88a87f4b4b67f47d4e0b45558
Mahe Tardy <mahe.tardy@gmail.com>
2024-04-23 18:41:18 +0200

    pkg/option: add EnableRuntimeSecurityPolicyCRD flag
    This flag allows to disable the RuntimeSecurityPolicyCRD (since it's
    enabled by default) to make it possible to run Tetragon in k8s context
    without the CRD.
    
    Note: now that we have multiple CRDs like that, we may want to group all
    of that behind the same "EnableCRDs" flags.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- pkg/option/flags.go
+++ pkg/option/flags.go
@@ -87,8 +87,9 @@ const (
 
 	KeyKmods = "kmods"
 
-	KeyEnablePodInfo          = "enable-pod-info"
-	KeyEnableTracingPolicyCRD = "enable-tracing-policy-crd"
+	KeyEnablePodInfo                  = "enable-pod-info"
+	KeyEnableTracingPolicyCRD         = "enable-tracing-policy-crd"
+	KeyEnableRuntimeSecurityPolicyCRD = "enable-runtime-security-policy-crd"
 
 	KeyExposeStackAddresses  = "expose-stack-addresses"
 	KeyExposeKernelAddresses = "expose-kernel-addresses"
@@ -169,6 +170,7 @@ func ReadAndSetFlags() error {
 
 	Config.EnablePodInfo = viper.GetBool(KeyEnablePodInfo)
 	Config.EnableTracingPolicyCRD = viper.GetBool(KeyEnableTracingPolicyCRD)
+	Config.EnableRuntimeSecurityPolicyCRD = viper.GetBool(KeyEnableRuntimeSecurityPolicyCRD)
 
 	Config.TracingPolicy = viper.GetString(KeyTracingPolicy)
 
@@ -279,6 +281,7 @@ func AddFlags(flags *pflag.FlagSet) {
 
 	flags.Bool(KeyEnablePodInfo, false, "Enable PodInfo custom resource")
 	flags.Bool(KeyEnableTracingPolicyCRD, true, "Enable TracingPolicy and TracingPolicyNamespaced custom resources")
+	flags.Bool(KeyEnableRuntimeSecurityPolicyCRD, true, "Enable RuntimeSecurityPolicy custom resources")
 
 	flags.Bool(KeyExposeKernelAddresses, false, "Expose real kernel addresses in events stack traces")
 	flags.Bool(KeyExposeStackAddresses, false, "Expose real linear addresses in events stack traces")
